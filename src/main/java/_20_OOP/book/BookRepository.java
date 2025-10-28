1de4090ae27e8266ebfa160f96d2dc94cc45dbbf
yang <doggabyyang@gmail.com>
2025-10-28 21:57:28 +0900

    dd

--- src/main/java/_20_OOP/book/BookRepository.java
+++ src/main/java/_20_OOP/book/BookRepository.java
@@ -1,62 +1,73 @@
 package _20_OOP.book;
-// 컨트롤 눌러서 메서드 누르면, 거기로 넘어간다
-// Repository의 역할 : Db와 연결관리
-// DB에서 데이터를 불러오는 역할
-public class BookRepository {
-    Book[] books;
 
+// repository 역할: DB와 연결 관리
+// DB 에서 데이터를 불러오는 역할
+public class BookRepository {
+    Book[] books; // book들을 저장할 배열(DB대용)
+
+    // 생성자를 통해서 의존하는 객체를 주입 -> 의존성 주입
     public BookRepository(Book[] books) {
         this.books = books;
     }
 
-    // 아래의 메서드들은 차후애 SQL쿼리가 되어야 한다
-    // sql쿼리 : database 언어
+    // 아래의 메서드들은 차후에 sql쿼리가 되어야 한다.
+    // sql쿼리: database 언어
 
+    // 도서를 배열에 저장하는 메서드
     void insert(Book book) {
-        //비어잇는 인덱스를 모름
+        // 비어있는 인덱스를 모름.
         int emptyIndex = getEmptyIndex();
         if (emptyIndex == -1) {
-            System.out.println("현재 저장할 공간이 없습니다");
+            System.out.println("현재 저장할 공간이 없습니다.");
             return;
         }
         books[emptyIndex] = book;
-        System.out.println(book.toString() + "저장완료");
+        System.out.println(book.toString() + "저장 완료");
     }
 
+    // null ? 참조자료형에만 있는 값
+    // 참조자료형 변수에 heap 주소가 저장되어야 하는데,
+    // 주소가 없는 경우가 있음(초기화가 안된 것)
+    // 저장된 주소가 없습니다를 표현하는 값.
+    // NullPointerException -> 참조자료형은 .으로 참조할 수 있는데
+    // 초기화가 안되면 null을 참조하게되어서 예외가 발생한다.
     int getEmptyIndex() {
-        for (int i = 0; i < books.length; i++) {
-            if (books[i] == null) {   //books는 어떻게 형성된것인가.,......
-                return i; // 이게 이제 getEmptyIndex를 통해서 emptyIndex로 값이 넘어간다
+        for(int i = 0; i < books.length; i++) {
+            if (books[i] == null) { // books의 i번째가 비어있으면
+                return i; // 해당 index를 반환
             }
         }
-        return -1; //0을 포함한 양수가 아닌경우
+        return -1; // 0을 포함한 양수가 아닌경우
     }
 
     // 등록된 도서들을 조회
-    // 배열의 5칸을 만들어놨는데, 만약 2권만 등록되었다면,?
-    // 배열의 길이만큼 순회하면, 에러가 발생할 수 있다
-    // 5칸중에 실제 등록된 도서만 따로 모아서 새로운 배열만 반환할 것이다
-    Book[] getBooksDatas() {
-        // count 변수 만들기
+    // 배열에 5칸을 만들어놨는데, 만약 2권만 등록됬다면?
+    // 배열의 길이만큼 순회하면 에러가 발생할 수 있음!
+    // 5칸중에 실제 등록된 도서만 모아서 새로운 배열을 반환
+    Book[] getBookDatas() {
         int count = 0;
         // 1. null이 아닌 도서의 갯수를 센다(count 활용)
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                count++;
+            }
+        }
         // 2. new Book[count]
-        // 3. new Book 에다가 복사
+        Book[] newBooks = new Book[count];
 
+        // 3. 등록된 도서만 복사
+        int j = 0; // newBooks의 index
+        for(int i = 0; i < books.length; i++) {
+            if (books[i] != null) {
+                newBooks[j] = books[i];
+                // j는 복사를 할때마다, 1씩증가(다음칸으로)
+                j++;
+            }
+        }
+
+        return newBooks;
     }
 
 
-
-
-    //지금까지 한 내용을 아직 다 못적음....
-    // 3.
-//     int j = 0;
-//     for (int i = 0; i<books.lenrth; i++){
-//        if (books[i] != null){
-//            newBooks[j] = books
-//        }
-//      }
-
-
-
 }
+
--- src/main/java/_21_Access/Main.java
+++ src/main/java/_21_Access/Main.java
@@ -1,19 +1,49 @@
 package _21_Access;
 
-import java.sql.SQLOutput;
+public class Access {
+    // 접근 제어자 - (클래스), 메서드, 필드 모두 동일
+    /*
+    - 접근제어자를 사용하는 이유?
+    데이터 보호: 주소로 직접참조하는걸 막기 위함 -> 메서드로 접근하게 강제
+    메서드로 접근 왜 함?: 검증된 값만 객체에 할당하려고
+    정해진 방법으로만 데이터를 변경하게 하여서 오류를 방지
 
-public class Main {
-    public static void main(String[] args) {
-        Access access = new Access("데이터 입니다");
-        System.out.println(access.getData());
+    - 접근제어자의 종류
+    1. public
+    - 가장 개방적, 어디서든 접근(".") 가능
+    - 외부에 완전히 공개되는 API등에 사용
 
-    // data의 접근제어자가 private가 되어서
-    // 직접참조(.)을 할 수 가 없다
+    2. protected
+    - 같은 패키지 내부 && 상속관계 일때만 접근(".") 가능
 
-    //access.data = "에러나라!"; //참조해서 변경
-    //System.out.println(access.data); //참조
-    // => private 으로하면 이렇게 불가능
+    3. default
+    - 같은 패키지 내부에서만 접근가능
+    - 접근제어자를 생략할때의 기본값
 
-    access.setData("메서드로만 변경가능");
+    4. private
+    - 가장 제한적. 클래스 내부가 아니면 접근 불가능
+    - 완전히 숨긴다(student.age, student.name 같은걸 불가능하게)
+    -> 메서드로 접근(메서드는 public이 되어야 된다)
+    */
+
+    // 접근제어자를 생략하면 default가 기본값이 된다.
+
+    private String data;
+
+    public Access(String data) {
+        this.data = data;
     }
+
+    // get필드명 : getter
+    public String getData() {
+        return data;
+    }
+
+    // set필드명 : setter
+    public void setData(String data) {
+        // 데이터 검증하는 코드가 들어감
+        this.data = data;
+    }
+
+
 }
